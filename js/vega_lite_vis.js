7893d51b2855ca8964f21cdf7cad114f3cea6a2e
Elizabeth Chai <echa0018@student.monash.edu>
2021-10-16 22:37:17 +1100

    cleaning up code

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -117,22 +117,3 @@ function choice(chosen) {
     }
     
 }
-
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-var navbar = document.getElementById("nav");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset <= 5000) {
-    navbar.classList.add("sticky")
-    alert("HELP")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
